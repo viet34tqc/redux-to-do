ff0a30aa6b6729c19b172753ef675d27f681cf09
viet34tqc <viet34tqc@gmail.com>
2021-04-21 17:18:38 +0700

    Setup tailwind

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    // If using custom config file
+    // Replace with tailwindcss: { config: './tailwindcss-config.js' },
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ tailwindcss-config.js
@@ -0,0 +1,13 @@
+// This is the custom config file, copy from tailwind.config.js into this and custmomize
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
